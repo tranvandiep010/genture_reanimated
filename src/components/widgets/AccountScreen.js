66390f0f79c04a2a23cac0cb45d7e67ae7270afc
Hùng Đỗ Vĩnh <hungdv@sonek.vn>
2020-09-26 17:14:15 +0700

    .

--- src/components/widgets/AccountScreen.js
+++ src/components/widgets/AccountScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import ViewPager from '@react-native-community/viewpager';
 
 export default class AccountScreen extends Component {
     constructor(props) {
@@ -8,17 +9,27 @@ export default class AccountScreen extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-
+            <View style={{ height: 300 }}>
+                <ViewPager style={[styles.viewPager, { height: 400 }]}
+                    initialPage={0}
+                    showPageIndicator={true}
+                    // keyboardDismissMode= {"on-drag"}
+                    transitionStyle="curl">
+                    <View key="1" style={{backgroundColor: 'red'}}>
+                        <Text style={{fontSize: 35}}>First page</Text>
+                        <Text style={{fontSize: 35}}>First page</Text>
+                    </View>
+                    <View key="2" style={{backgroundColor: 'blue'}}>
+                        <Text style={{fontSize: 35}}>Second page</Text>
+                    </View>
+                </ViewPager>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'blue',
-        width: '100%',
-        height: '100%'
-    }
-})
+    viewPager: {
+        flex: 1,
+    },
+});
